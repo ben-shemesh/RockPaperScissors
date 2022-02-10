057f63f7bd26edf4628bd7932e2d64d76eaebc5b
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-02-10 14:01:19 -0500

    Improved functionality of playerPick function.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function computerplay() {
+function computerPlay() {
   let computerNumber = Math.floor(Math.random() * 3) + 1;
   switch (computerNumber) {
     case 1:
@@ -10,22 +10,32 @@ function computerplay() {
     case 3:
       return `Scissors`;
   }
-  return computerNumber;
 }
+console.log(computerPlay());
+//
 function playerPlay() {
-  let playerPick = prompt(`Choose your weapon: "Rock" "Paper" or "Scissors".`);
-  if (
-    playerPick != `Rock` ||
-    playerPick != `Paper` ||
-    playerPick != `Scissors`
-  ) {
-    return `Please choose either "Rock", "Paper" or "Scissors" (this is case sensitive).`;
-  } else {
-    return playerPick;
+  function playerPlay() {
+    let playerPick = prompt(
+      `Choose your weapon: "Rock" "Paper" or "Scissors".`
+    ).toUpperCase();
+    switch (playerPick) {
+      case `ROCK` || `PAPER` || `SCISSORS`:
+        return playerPick;
+        break;
+    }
+    if(playerPick != `ROCK` || playerPick != `PAPER`|| playerPick != `SCISSORS`) {
+      while (true){
+        let playerPick = prompt(`select again`).toUpperCase()
+        if (playerPick == `ROCK` || playerPick == `PAPER` || playerPick == `SCISSORS`) {
+          return playerPick
+        }
+      }
+    }
   }
-}
-function playRound(computerSelection, playerSelection) {
-  let computerSelection = computerplay();
-  let playerSelection = playerPick();
-}
-console.log(playerPlay());
+
+// console.log(playerPlay());
+// function playRound(computerSelection, playerSelection) {
+//   let computerSelection = computerPlay();
+//   let playerSelection = playerPick();
+// }
+// console.log(computerPlay());
