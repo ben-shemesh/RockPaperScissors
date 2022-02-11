0695ea7ea03a7bb43f1b266adf9250438155c2b3
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-02-10 20:13:47 -0500

    completed all functions. Added alert() function to display round information to user.

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
+let round = 1;
+let userScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   let computerNumber = Math.floor(Math.random() * 3) + 1;
   switch (computerNumber) {
     case 1:
-      return `Rock`;
+      return `ROCK`;
       break;
     case 2:
-      return `Paper`;
+      return `PAPER`;
       break;
     case 3:
-      return `Scissors`;
+      return `SCISSORS`;
   }
 }
-console.log(computerPlay());
-//
 function playerPlay() {
-  function playerPlay() {
-    let playerPick = prompt(
-      `Choose your weapon: "Rock" "Paper" or "Scissors".`
-    ).toUpperCase();
-    switch (playerPick) {
-      case `ROCK` || `PAPER` || `SCISSORS`:
-        return playerPick;
-        break;
-    }
-    if(playerPick != `ROCK` || playerPick != `PAPER`|| playerPick != `SCISSORS`) {
-      while (true){
-        let playerPick = prompt(`select again`).toUpperCase()
-        if (playerPick == `ROCK` || playerPick == `PAPER` || playerPick == `SCISSORS`) {
-          return playerPick
-        }
-      }
-    }
-  }
+  let playerPick = prompt(
+    `Choose your weapon: "Rock" "Paper" or "Scissors".`
+  ).toUpperCase();
+  return playerPick;
+}
 
-// console.log(playerPlay());
-// function playRound(computerSelection, playerSelection) {
-//   let computerSelection = computerPlay();
-//   let playerSelection = playerPick();
-// }
-// console.log(computerPlay());
+function playRound(computerSelection, playerSelection) {
+  computerSelection = computerPlay();
+  playerSelection = playerPlay();
+  if (computerSelection == playerSelection) {
+    alert(
+      `It's a tie 🪢 : Your Score: ${userScore}, Computer's Score: ${computerScore}. It's the ${round} round.`
+    );
+  } else if (
+    (playerSelection == `ROCK` && computerSelection == `PAPER`) ||
+    (playerSelection == "PAPER" && computerSelection == `SCISSORS`) ||
+    (playerSelection == `SCISSORS` && computerSelection == `PAPER`)
+  ) {
+    userScore++,
+      alert(
+        `You win 😃 : Your Score: ${userScore}, Computer's Score: ${computerScore}. It's the ${round} round.`
+      );
+  } else {
+    computerScore++,
+      alert(
+        `You loose 😞 : Your Score: ${userScore}, Computer's Score: ${computerScore}. It's the ${round} round.`
+      );
+  }
+}
+function game() {
+  do {
+    playRound();
+    round++;
+  } while (round <= 5);
+}
+game();
